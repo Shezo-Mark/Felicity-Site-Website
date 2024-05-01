674be0ef16223748e8cf4086941f318a482add2b
shehzad <shehzadamin697@gmail.com>
2024-05-01 15:44:08 +0500

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-
-});// End of document ready function
-$(document).ready(function () {
-    $("#owl_testimonials").owlCarousel({
-        items: 1,
-        loop: true,
-        dots:true,
-        nav: true,
-        navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-    });
-});
-
-
 $('.our_team').owlCarousel({
     loop:true,
     margin:10,
@@ -57,3 +44,17 @@ $(".product-carousel").owlCarousel({
         }
     }
 })
+
+$('.testimonials_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:3,
+    // navText: ['<img class="img-fluid" src="../../images/slide-right-arr.png">', '<i class="fas fa-chevron-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
+});
